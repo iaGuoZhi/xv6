f49065007d8a6def3ae3dbd64adb076b4a72ed08
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-02-25 22:53:49 +0800

    pass xargs

--- Makefile
+++ Makefile
@@ -142,6 +142,7 @@ UPROGS=\
 	$U/_sleep\
 	$U/_pingpong\
 	$U/_primes\
+	$U/_xargs\
 	$U/_find\
 	$U/_ln\
 	$U/_ls\
--- user/xargs.c
+++ user/xargs.c
@@ -2,43 +2,50 @@
 #include "kernel/param.h"
 #include "user/user.h"
 
+int append_argv(char **exec_argv, int *argv_idx, char *buf)
+{
+    char c;
+    int i = 0;
+
+    if(read(0, &c, sizeof(char)) == 0)
+        return 0;
+
+    if(c == '\0')
+        return 0;
+
+    while(c != '\n'){
+        if(c != ' '){
+            buf[i] = c;
+            if(i == 0 || buf[i - 1] == ' '){
+                exec_argv[(*argv_idx)] = &(buf[i]);
+                *argv_idx = *argv_idx + 1;
+            }
+            i += 1;
+        }
+        if(read(0, &c, sizeof(char)) == 0)
+            return 0;
+    }
+    buf[i] = '\0';
+
+    return 1;
+}
+
+
 int main(int argc, char **argv) {
     int argv_idx = 0, t;
     int pid, xstatus;
     int i;
     char *exec_argv[MAXARG]; 
-    char buf[512], c = ' ';
+    char buf[512]; 
 
     for(i = 1; i < argc; i++){
         exec_argv[argv_idx++] = argv[i];
     }
 
-    while(c != '\0'){
-        i = -1;
+    while(1){
         t = argv_idx;
-        while(c != '\0'){
-            read(0, &c, sizeof(char));
-            if(c == '\0')
-                break;
-            printf("c: %c\n", c);
-            if(i >= 0 && buf[i] != '\0'){
-                if(c != ' '){
-                    buf[++i] = c;
-                }else{
-                    buf[++i] = '\0';
-                }
-            }else{
-                if(c != ' '){
-                    buf[++i] = c;
-                    exec_argv[t++] = &(buf[i]);
-                }
-            }
-        } 
-        if(i == -1)
+        if(!append_argv(exec_argv, &t, buf)){
             break;
-        buf[i] = '\0';
-        for(i = 0; i < t; ++i){
-            printf("argv[%d]: %s\n", i,  exec_argv[i]);
         }
         
         // fork + exec
@@ -48,7 +55,7 @@ int main(int argc, char **argv) {
             exit(1);
         }
         if(pid == 0){
-            if(exec(exec_argv[0], &(exec_argv[1])) < 0){
+            if(exec(exec_argv[0], &(exec_argv[0])) < 0){
                 printf("exec failed\n");
                 exit(1);
             }
