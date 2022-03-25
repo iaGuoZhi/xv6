772d059b251e298fa545180dcb7d8c905d41b414
Guo Zhi <qtxuning1999@sjtu.edu.cn>
2022-03-25 23:11:14 +0800

    pass backtrace

--- kernel/defs.h
+++ kernel/defs.h
@@ -157,6 +157,7 @@ void            trapinit(void);
 void            trapinithart(void);
 extern struct spinlock tickslock;
 void            usertrapret(void);
+void            backtrace(void);
 
 // uart.c
 void            uartinit(void);
